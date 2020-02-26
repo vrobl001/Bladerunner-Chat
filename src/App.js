a11fd8977bdb75e765e1dbfa4a83213426c1469e
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 13:12:19 -0500

    imported home comp and created a switch route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Reusable components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
+// Page components
+import Home from './pages/Home/Home';
+
 import './App.css';
 
 function App() {
     return (
         <div className='App-outer-container'>
             <Navbar />
-            <div className='App-inner-container'></div>
+            <div className='App-inner-container'>
+                <Switch>
+                    <Route path='/' render={props => <Home />} />
+                </Switch>
+            </div>
             <Footer />
         </div>
     );
