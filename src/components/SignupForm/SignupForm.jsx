c477e4bcb4e504d18a0343a4de0084bfd6b3f592
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 13:55:07 -0500

    created signup form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -4,11 +4,26 @@ class SignupForm extends Component {
     render() {
         return (
             <form>
-                <input type='text' />
-                <input type='email' />
-                <input type='password' />
-                <input type='password' />
-                <button type='submit'>Submit</button>
+                <fieldset>
+                    <legends>Signup Form</legends>
+                    <label htmlFor='name'>Full Name</label>
+                    <input id='name' name='name' type='text' />
+
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' name='email' type='email' />
+
+                    <label htmlFor='password'>Password</label>
+                    <input id='password' name='password' type='password' />
+
+                    <label htmlFor='passwordConf'>Password Confirmation</label>
+                    <input
+                        id='passwordConf'
+                        name='passwordConf'
+                        type='password'
+                    />
+
+                    <button type='submit'>Submit</button>
+                </fieldset>
             </form>
         );
     }
