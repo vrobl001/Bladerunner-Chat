55db9e4fb5dd62635cb345acf9b8fe20fa6d4b28
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 13:36:03 -0500

    boilerplate

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+const Login = props => {
+    return <h1>Login</h1>;
+};
+
+export default Login;
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Signup.module.css';
+
+const Signup = props => {
+    return <h1>Signup</h1>;
+};
+
+export default Signup;
