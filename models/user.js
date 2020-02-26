9d0ffb5fb72f42f30fb0800cf73568601f2a2575
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 16:22:27 -0500

    creates user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        name: String,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: String
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
