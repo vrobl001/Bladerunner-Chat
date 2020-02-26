e3d6dd2799ecfdcc3e0a902d49d8c4f9f17baa3b
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 16:34:01 -0500

    created hook that checks pw mod, salts, and hash

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema(
     {
@@ -14,5 +17,15 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+    });
+});
 
 module.exports = mongoose.model('User', userSchema);
