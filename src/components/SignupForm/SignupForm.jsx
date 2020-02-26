85c5b350155e5f48c325671753d97e009cd35bbe
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 13:58:02 -0500

    initialized state

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class SignupForm extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        passwordConf: ''
+    };
     render() {
         return (
             <form>
