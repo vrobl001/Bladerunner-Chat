22dbe0a8389b2366f3eca13cce90cec7b326cff2
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 13:59:20 -0500

    value is now controlled by state

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -13,19 +13,35 @@ class SignupForm extends Component {
                 <fieldset>
                     <legends>Signup Form</legends>
                     <label htmlFor='name'>Full Name</label>
-                    <input id='name' name='name' type='text' />
+                    <input
+                        id='name'
+                        name='name'
+                        type='text'
+                        value={this.state.name}
+                    />
 
                     <label htmlFor='email'>Email</label>
-                    <input id='email' name='email' type='email' />
+                    <input
+                        id='email'
+                        name='email'
+                        type='email'
+                        value={this.state.email}
+                    />
 
                     <label htmlFor='password'>Password</label>
-                    <input id='password' name='password' type='password' />
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                    />
 
                     <label htmlFor='passwordConf'>Password Confirmation</label>
                     <input
                         id='passwordConf'
                         name='passwordConf'
                         type='password'
+                        value={this.state.passwordConf}
                     />
 
                     <button type='submit'>Submit</button>
