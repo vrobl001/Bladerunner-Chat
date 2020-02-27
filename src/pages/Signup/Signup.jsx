9489402ac98dbdd6f7fc3b89287796f8b58c1f36
Vincent Robles <sylkisgsx@gmail.com>
2020-02-27 00:25:05 -0500

    removed css import and destructured drilled props

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import SignupForm from '../../components/SignupForm/SignupForm';
-import styles from './Signup.module.css';
 
 const Signup = props => {
     return (
         <main>
             <h1>Signup</h1>
-            <SignupForm />
+            <SignupForm {...props} />
         </main>
     );
 };
