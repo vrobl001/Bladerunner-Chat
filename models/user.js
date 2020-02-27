d1f67ce0834701e0e706a64595e91f97e3fa5314
Vincent Robles <sylkisgsx@gmail.com>
2020-02-27 01:05:13 -0500

    prevents access of private info

--- models/user.js
+++ models/user.js
@@ -18,6 +18,15 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+        delete ret.__v;
+    }
+});
+
 userSchema.pre('save', function(next) {
     const user = this;
     if (!user.isModified('password')) return next();
