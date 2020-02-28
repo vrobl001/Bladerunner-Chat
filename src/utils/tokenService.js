eda9365db4bb9b0246be046df4132d028e8a8344
Vincent Robles <sylkisgsx@gmail.com>
2020-02-28 11:40:21 -0500

    grabs user from token if not expired

--- src/utils/tokenService.js
+++ src/utils/tokenService.js
@@ -12,6 +12,7 @@ function getToken() {
 
 function getUserFromToken() {
     const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
 
 function setToken(token) {
