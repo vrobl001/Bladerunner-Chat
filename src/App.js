0b4f42e090a83dd4a5c55766a2a752e0014d59cd
Vincent Robles <sylkisgsx@gmail.com>
2020-02-28 11:15:14 -0500

    added handlesignuporlogin

--- src/App.js
+++ src/App.js
@@ -12,8 +12,17 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
 import './App.css';
+import userService from './utils/userService';
 
 class App extends Component {
+    state = {
+        user: userService.getUser()
+    };
+
+    handleSignupOrLogin = () => {
+        this.setState({ user: userService.getUser() });
+    };
+
     render() {
         return (
             <div className='App-outer-container'>
