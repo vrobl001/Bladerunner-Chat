b63c649b7cba0030ac42e039c36d4ad15deb3de2
Vincent Robles <sylkisgsx@gmail.com>
2020-02-28 12:27:50 -0500

    changed nav li to conditional ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import userService from '../../utils/userService';
 import styles from './Navbar.module.css';
 
 const Navbar = props => {
+    const conditionalUI = userService.getUser() ? (
+        <>
+            <li>
+                <Link to='/chatrooms'>Chat Rooms</Link>
+            </li>
+            <li>
+                <Link>Logout</Link>
+            </li>
+        </>
+    ) : (
+        <>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+            <li>
+                <Link to='signup'>Signup</Link>
+            </li>
+        </>
+    );
     return (
         <nav className={styles.navbar}>
             <Link to='/'>
                 <h1>Blade Runner Chat</h1>
             </Link>
-            <ul>
-                <li>
-                    <Link to='/chatrooms'>Chat Rooms</Link>
-                </li>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-                <li>
-                    <Link to='signup'>Signup</Link>
-                </li>
-            </ul>
+            <ul></ul>
         </nav>
     );
 };
