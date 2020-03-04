41d29880a99e5ba285a4f9937f993a7f7fd9615a
Vincent Robles <sylkisgsx@gmail.com>
2020-03-04 00:48:29 -0500

    refactored so it's not a class

--- src/components/Messenger/RetrieveMessages/RetrieveMessages.jsx
+++ src/components/Messenger/RetrieveMessages/RetrieveMessages.jsx
@@ -1,48 +1,24 @@
-import React, { Component } from 'react';
-import messageService from '../../../utils/messageService';
+import React from 'react';
 import styles from './RetrieveMessages.module.css';
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:4000');
 
-class RetrieveMessages extends Component {
-    state = {
-        chatTopic: '',
-        name: '',
-        msg: ''
-    };
-    async componentDidMount() {
-        const retrieveMessages = await messageService.retrieveMessages();
-        this.props.handleUpdateMessages(retrieveMessages);
-        socket.on('sendMessages', data => {
-            this.props.handleUpdateMessages(data);
-        });
-    }
-
-    render() {
-        const allMessages = this.props.messages.map((message, idx) => (
-            <tr key={idx}>
-                <td>{message.name}</td>
-                <td>{message.msg}</td>
-            </tr>
-        ));
-        return (
-            <div className={styles.rmContainer} id='containsMessages'>
-                {this.props.messages.length ? (
-                    <table id='test' className={styles.table}>
-                        <tbody>
-                            {allMessages}
-                            <tr>
-                                <td>{this.state.name}</td>
-                                <td>{this.state.msg}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                ) : (
-                    <h4 className='text-info'>No Messages Yet!</h4>
-                )}
-            </div>
-        );
-    }
-}
+const RetrieveMessages = props => {
+    const allMessages = props.messages.map((message, idx) => (
+        <tr key={idx}>
+            <td>{message.name}</td>
+            <td>{message.msg}</td>
+        </tr>
+    ));
+    return (
+        <div className={styles.rmContainer} id='containsMessages'>
+            {props.messages.length ? (
+                <table className={styles.table}>
+                    <tbody>{allMessages}</tbody>
+                </table>
+            ) : (
+                <h4>No Messages Yet!</h4>
+            )}
+        </div>
+    );
+};
 
 export default RetrieveMessages;
