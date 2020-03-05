b663e6a23bc45fe52336581f73d1f025e98697ee
Vincent Robles <sylkisgsx@gmail.com>
2020-03-05 04:12:34 -0500

    passed props to show online users

--- src/components/Messenger/OnlineUsers/OnlineUsers.jsx
+++ src/components/Messenger/OnlineUsers/OnlineUsers.jsx
@@ -3,16 +3,13 @@ import Paper from '@material-ui/core/Paper';
 import styles from './OnlineUsers.module.css';
 
 const OnlineUsers = props => {
+    const allUsers = props.online.map((user, idx) => (
+        <li key={idx}>{user.name}</li>
+    ));
     return (
         <div className={styles.ouContainer}>
             <Paper elevation={10} className={styles.ouInnerContainer}>
-                <ul>
-                    <li>{props.user.name}</li>
-                    <li>brian</li>
-                    <li>crystal</li>
-                    <li>lina</li>
-                    <li>noor</li>
-                </ul>
+                <ul>{allUsers}</ul>
             </Paper>
         </div>
     );
