a9df211b27b36ac01c8cdca01d470db416812fd3
Vincent Robles <sylkisgsx@gmail.com>
2020-03-05 04:12:58 -0500

    completed homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../../logo.svg';
+import styles from './Home.module.css';
+import Paper from '@material-ui/core/Paper';
 
 const Home = props => {
     return (
-        <main>
-            <h1>Homepage</h1>
+        <main className={styles.homeMain}>
+            <div className={styles.homeContainer}>
+                <h1>Chat App Powered By React</h1>
+
+                <img src={logo} className={styles.reactLogo} alt='react logo' />
+
+                <h3>Vincent Robles</h3>
+            </div>
         </main>
     );
 };
