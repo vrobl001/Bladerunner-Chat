dcfe0a339e2c79b407f96131286ae69960fd8958
Vincent Robles <sylkisgsx@gmail.com>
2020-03-04 19:54:47 -0500

    protected send message route

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const msgsCtrl = require('../../controllers/messages');
 
-router.post('/chatrooms', msgsCtrl.sendMessages);
-router.get('/chatrooms', msgsCtrl.retrieveMessages);
+router.use(require('../../config/auth'));
+
+router.post('/chatrooms', isAuthenticated, msgsCtrl.sendMessages);
+router.get('/chatrooms', isAuthenticated, msgsCtrl.retrieveMessages);
+
+// Helper functions
+function isAuthenticated(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({ msg: 'not authorized' });
+}
 
 module.exports = router;
