66f473a90d14ebf590d981f3c787631b330c4b97
Vincent Robles <sylkisgsx@gmail.com>
2020-03-06 17:12:01 -0500

    removed socket to see if it was the problem

--- server.js
+++ server.js
@@ -5,28 +5,6 @@ const path = require('path');
 
 const app = express();
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-io.on('connection', socket => {
-    socket.on('newUser', user => {
-        console.log('this person is logged on ', user);
-        io.emit('newUser', user);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    socket.on('sendMessages', messages => {
-        io.emit('sendMessages', messages);
-    });
-});
-
-http.listen(9000, () => {
-    console.log('Socket.io is listening on port 4000');
-});
-
 require('dotenv').config();
 require('./config/database');
 
