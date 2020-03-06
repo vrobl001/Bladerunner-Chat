f133a9c6f49d1fff56e34cd503d4ca17819ea3a8
Vincent Robles <sylkisgsx@gmail.com>
2020-03-06 00:22:04 -0500

    updated homepage styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,15 @@ import styles from './Home.module.css';
 
 const Home = props => {
     return (
-        <main className={styles.homeMain}>
-            <div className={styles.homeContainer}>
+        <div className={styles.homeContainer}>
+            <div className={styles.homeContent}>
                 <h1>Chat App Powered By React</h1>
 
                 <img src={logo} className={styles.reactLogo} alt='react logo' />
 
                 <h3>Vincent Robles</h3>
             </div>
-        </main>
+        </div>
     );
 };
 
