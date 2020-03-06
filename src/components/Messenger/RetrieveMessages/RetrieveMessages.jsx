0cacbcd5ffa8f9e4da80c2d4725298f4b9a7e59d
Vincent Robles <sylkisgsx@gmail.com>
2020-03-06 15:46:51 -0500

    added more styling to chat boxes

--- src/components/Messenger/RetrieveMessages/RetrieveMessages.jsx
+++ src/components/Messenger/RetrieveMessages/RetrieveMessages.jsx
@@ -5,19 +5,37 @@ const RetrieveMessages = props => {
     const filteredMessages = props.messages.filter(message => {
         return message.chatTopic === props.chatTopic;
     });
-    const mappedFilteredMessages = filteredMessages.map((message, idx) => (
-        <div key={idx}>
-            <div className={styles.displayedMessages}>
-                <div>
-                    <p className={styles.rmChatTopic}>{message.chatTopic}</p>
-                </div>
-                <div className={styles.test}>
-                    <p className={styles.rmUser}>{message.name}</p>
-                    <p className={styles.rmMessage}>{message.msg}</p>
+    const mappedFilteredMessages = filteredMessages.map((message, idx) =>
+        props.user.name === message.name ? (
+            <div className={styles.userContainer} key={idx}>
+                <div className={styles.displayedMessages}>
+                    <div>
+                        <p className={styles.rmChatTopic}>
+                            {message.chatTopic}
+                        </p>
+                    </div>
+                    <div className={styles.nameAndMessage}>
+                        <p className={styles.rmUser}>{message.name}</p>
+                        <p className={styles.rmMessage}>{message.msg}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
+        ) : (
+            <div key={idx}>
+                <div className={styles.displayedMessages}>
+                    <div>
+                        <p className={styles.rmChatTopic}>
+                            {message.chatTopic}
+                        </p>
+                    </div>
+                    <div className={styles.nameAndMessage}>
+                        <p className={styles.rmUser}>{message.name}</p>
+                        <p className={styles.rmMessage}>{message.msg}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    );
     return (
         <div className={styles.rmContainer}>
             {filteredMessages.length ? (
@@ -25,7 +43,9 @@ const RetrieveMessages = props => {
                     <div>{mappedFilteredMessages}</div>
                 </div>
             ) : (
-                <h1>No Messages Yet!</h1>
+                <h1 className={styles.noMessage}>
+                    Be the first to start a conversation!
+                </h1>
             )}
         </div>
     );
