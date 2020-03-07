ed2147be2b9ef8dda41f4bb89a220dbb498577da
Vincent Robles <sylkisgsx@gmail.com>
2020-03-06 20:49:29 -0500

    styling

--- src/pages/ChatRooms/ChatRooms.jsx
+++ src/pages/ChatRooms/ChatRooms.jsx
@@ -4,20 +4,15 @@ import RetrieveMessages from '../../components/Messenger/RetrieveMessages/Retrie
 import SendMessages from '../../components/Messenger/SendMessages/SendMessages';
 import OnlineUsers from '../../components/Messenger/OnlineUsers/OnlineUsers';
 import styles from './ChatRooms.module.css';
-import Paper from '@material-ui/core/Paper';
 
 const ChatRooms = props => {
     return (
         <div className={styles.crOuterContainer}>
             <ChatTopics {...props} />
-
             <div className={styles.crInnerContainer}>
-                <Paper elevation={10}>
-                    <RetrieveMessages {...props} />
-                    <SendMessages {...props} />
-                </Paper>
+                <RetrieveMessages {...props} />
+                <SendMessages {...props} />
             </div>
-
             <OnlineUsers {...props} />
         </div>
     );
