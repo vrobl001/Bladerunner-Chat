407f10a753631c997d1fbfecb5bcd1304142354e
Vincent Robles <sylkisgsx@gmail.com>
2020-03-07 10:53:33 -0500

    removed unneeded offline user method

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
     };
 
     handleLogout = () => {
-        this.handleOfflineUser();
         userService.logout();
         this.setState({
             user: userService.getUser(),
