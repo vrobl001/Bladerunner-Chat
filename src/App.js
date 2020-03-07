4433f83324a36634afd33993128ea961a31238d7
Vincent Robles <sylkisgsx@gmail.com>
2020-03-07 04:09:19 -0500

    changed port for socket back to 5000

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './App.css';
 import userService from './utils/userService';
 
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:80');
+const socket = openSocket('http://localhost:5000');
 
 class App extends Component {
     state = {
