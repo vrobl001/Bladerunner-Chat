5419ba3d1de6c3bf1be9223f38aedd10868a4146
Vincent Robles <sylkisgsx@gmail.com>
2020-03-07 12:45:18 -0500

    heroku test

--- src/components/Messenger/SendMessages/SendMessages.jsx
+++ src/components/Messenger/SendMessages/SendMessages.jsx
@@ -3,7 +3,7 @@ import messageService from '../../../utils/messageService';
 import styles from './SendMessages.module.css';
 import Paper from '@material-ui/core/Paper';
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:5000');
+const socket = openSocket('https://blade-runner-chat.herokuapp.com/chatrooms');
 
 class SendMessages extends Component {
     state = this.getInitialState();
