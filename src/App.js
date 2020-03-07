288eecc11d9b56184e19cb9e543aea83457fd709
Vincent Robles <sylkisgsx@gmail.com>
2020-03-07 04:01:58 -0500

    final changes to socket

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './App.css';
 import userService from './utils/userService';
 
 import openSocket from 'socket.io-client';
-const socket = openSocket(':80');
+const socket = openSocket('http://localhost:80');
 
 class App extends Component {
     state = {
