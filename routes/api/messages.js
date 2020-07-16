79ef03b25de5b580d9313d4cc885512648d5ce51
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:40:56 -0400

    updated routes

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -3,13 +3,12 @@ const msgsCtrl = require('../../controllers/messages');
 
 router.use(require('../../config/auth'));
 
-router.post('/chatrooms', isAuthenticated, msgsCtrl.sendMessages);
-router.get('/chatrooms', isAuthenticated, msgsCtrl.retrieveMessages);
+router.post('/', isAuthenticated, msgsCtrl.sendMessages);
+router.get('/', isAuthenticated, msgsCtrl.retrieveMessages);
 
-// Helper functions
 function isAuthenticated(req, res, next) {
-    if (req.user) return next();
-    return res.status(401).json({ msg: 'not authorized' });
+  if (req.user) return next();
+  return res.status(401).json({ msg: 'not authorized' });
 }
 
 module.exports = router;
