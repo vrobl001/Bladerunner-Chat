8b164d9b3d89061d03d44b53497cde69db3b014e
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:48:33 -0400

    home page lists all chat rooms

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import ChatRooms from '../../components/ChatRooms/ChatRooms';
 import styles from './Home.module.css';
 
-const Home = props => {
-    return (
-        <div className={styles.homeContainer}>
-            <div className={styles.homeContent}>
-                <h1>Chat App Powered By React</h1>
-
-                <img src={logo} className={styles.reactLogo} alt='react logo' />
-
-                <h3>Vincent Robles</h3>
-            </div>
-        </div>
-    );
-};
-
-export default Home;
+export default function Home(props) {
+  return (
+    <div className={styles.homeContainer}>
+      <ChatRooms {...props} />
+    </div>
+  );
+}
