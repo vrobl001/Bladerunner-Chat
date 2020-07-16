cb301b4ce47bcadce919475fc215e8670ad6a414
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:41:51 -0400

    added chatrooms component

---
+++ src/components/ChatRooms/ChatRooms.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './ChatRooms.module.css';
+
+const ChatRooms = (props) => {
+  const allChatRooms = props.chatRooms.map((app, idx) => (
+    <li key={idx} onClick={props.handleUpdateChatTopic}>
+      <Link
+        className={styles.chatRoomWrapper}
+        name={app.name}
+        data-idx={idx}
+        to='/chat'
+        onClick={props.handleActiveApp}>
+        <div className={styles.chatRooms}>
+          <i className='material-icons'>{app.icon}</i>
+          <p>{app.name}</p>
+        </div>
+      </Link>
+    </li>
+  ));
+
+  return <div className={styles.chatRoomsContainer}>{allChatRooms}</div>;
+};
+
+export default ChatRooms;
