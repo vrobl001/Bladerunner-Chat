0b12ab41f9c9be90351d65100704c7ef406acc4f
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:49:43 -0400

    changed to proper convention

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import SignupForm from '../../components/SignupForm/SignupForm';
 import styles from './Signup.module.css';
 
-const Signup = props => {
-    return (
-        <main className={styles.signupContainer}>
-            <div>
-                <SignupForm
-                    {...props}
-                    handleSignupOrLogin={props.handleSignupOrLogin}
-                />
-            </div>
-        </main>
-    );
-};
-
-export default Signup;
+export default function Signup(props) {
+  return (
+    <div className={styles.signupFormContainer}>
+      <SignupForm {...props} />
+    </div>
+  );
+}
