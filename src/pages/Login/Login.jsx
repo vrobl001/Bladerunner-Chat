338e584c7245503585348a5a8dab82da2a7dfcbc
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:49:32 -0400

    changed to proper convention

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import styles from './Login.module.css';
 
-const Login = props => {
-    return (
-        <main className={styles.loginContainer}>
-            <div>
-                <LoginForm {...props} />
-            </div>
-        </main>
-    );
-};
-
-export default Login;
+export default function Login(props) {
+  return (
+    <div className={styles.loginFormContainer}>
+      <LoginForm {...props} />
+    </div>
+  );
+}
