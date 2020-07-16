e4db9de038fc6d0b792abcdc755134736079c0b5
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:48:50 -0400

    included socket in message service

--- src/utils/messageService.js
+++ src/utils/messageService.js
@@ -1,40 +1,49 @@
 import tokenService from './tokenService';
-const BASE_URL = '/api/messages/chatrooms';
+import openSocket from 'socket.io-client';
+const socket = openSocket();
+const BASE_URL = '/api/messages/';
 
 function sendMessages(message) {
-    return fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'Application/json',
-            Authorization: 'Bearer ' + tokenService.getToken()
-        },
-        body: JSON.stringify(message)
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('could not post message');
-        }
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'Application/json',
+      Authorization: 'Bearer ' + tokenService.getToken(),
+    },
+    body: JSON.stringify(message),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Could not send message!');
+      }
+    })
+    .then(() => {
+      const name = message.name;
+      const chatTopic = message.chatTopic;
+      const msg = message.msg;
+      socket.emit('sendMessages', { name, chatTopic, msg });
     });
 }
 
 function retrieveMessages() {
-    return fetch(BASE_URL, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'Application/json',
-            Authorization: 'Bearer ' + tokenService.getToken()
-        }
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('could not retrieve message');
-        }
-    });
+  return fetch(BASE_URL, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'Application/json',
+      Authorization: 'Bearer ' + tokenService.getToken(),
+    },
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Could not retrieve message!');
+    }
+  });
 }
 
 export default {
-    sendMessages,
-    retrieveMessages
+  sendMessages,
+  retrieveMessages,
 };
