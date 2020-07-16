38f912988cb340c60cd96efdd88ac824870e2584
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 16:40:32 -0400

    changed order

--- models/message.js
+++ models/message.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema(
-    {
-        chatTopic: {
-            type: String,
-            required: true
-        },
-        name: {
-            type: String,
-            required: true
-        },
-        msg: {
-            type: String,
-            required: true
-        }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    chatTopic: {
+      type: String,
+      required: true,
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Message', messageSchema);
